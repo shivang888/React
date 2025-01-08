f415cc5c50b4d3c288fa207233fed0f218d3fec0
shivang888 <shivangkad9@gmail.com>
2025-01-08 11:54:11 +0530

    React file add

---
+++ Practis/Day3/Day3/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  return(
+    <div>
+      <h1>Shivang Rayka</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpi03rAgYu115VigbBOVRNEm_vXc2ZtZMrXg&s" alt="" />
+      <p>Shivang Rayka is Biggest Businessmen on World</p>
+    </div>
+  )
+}
+
+export default App;
